c283701a9e80ed7e146e5d89c7e839347a997727
jtoufik <112856590+jtoufik@users.noreply.github.com>
2023-06-21 22:45:46 +0200

    arrow animatie toegevoegd

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,48 +6,56 @@ toggleButton.addEventListener('click', function() {
 });
 
 function initMap() {
-    const mapOptions = {
-        center: { lat: 33.986261, lng: -118.438543 },
-        zoom: 7,
-        styles: [
-            { featureType: 'water', elementType: 'geometry', stylers: [{ color: '#023A57' }] },
-            { featureType: 'landscape', elementType: 'geometry', stylers: [{ color: '#8AD3DC' }] },
-            { featureType: 'administrative', elementType: 'geometry.fill', stylers: [{ color: '#1C526C' }] },
-            { featureType: 'poi.park', elementType: '.geometry.labels.text.fill', stylers: [{ visibility: 'off'  }] },
-            { featureType: 'road', elementType: 'geometry', stylers: [{ visibility: 'off' }] },
-            { featureType: 'transit', elementType: 'labels.icon', stylers: [{ visibility: 'off' }] },
-            { featureType: 'transit', elementType: 'labels.text.fill', stylers: [{ visibility: 'off' }] },
-            { featureType: 'road.highway', elementType: 'labels', stylers: [{ visibility: 'off' }] }
-        ]
-    };
+  const mapOptions = {
+    center: { lat: 33.986261, lng: -118.438543 },
+    zoom: 7,
+    styles: [
+      { featureType: 'water', elementType: 'geometry', stylers: [{ color: '#023A57' }] },
+      { featureType: 'landscape', elementType: 'geometry', stylers: [{ color: '#8AD3DC' }] },
+      { featureType: 'administrative', elementType: 'geometry.fill', stylers: [{ color: '#1C526C' }] },
+      { featureType: 'poi.park', elementType: '.geometry.labels.text.fill', stylers: [{ visibility: 'off'  }] },
+      { featureType: 'road', elementType: 'geometry', stylers: [{ visibility: 'off' }] },
+      { featureType: 'transit', elementType: 'labels.icon', stylers: [{ visibility: 'off' }] },
+      { featureType: 'transit', elementType: 'labels.text.fill', stylers: [{ visibility: 'off' }] },
+      { featureType: 'road.highway', elementType: 'labels', stylers: [{ visibility: 'off' }] }
+    ],
+    mapTypeControlOptions: {
+      mapTypeIds: [
+        google.maps.MapTypeId.ROADMAP // Include only the ROADMAP map type
+      ]
+    },
+    streetViewControl: false, // Disable the Street View control
+    mapTypeControl: false // Disable the map type control
 
-    const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  };
 
-    const markerOptions = {
-        position: { lat: 33.959874, lng: -118.459416 },
-        map: map,
-        title: 'Interceptor 007',
-        label: '1',
-        draggable: true
-    };
+  const map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-    const marker = new google.maps.Marker(markerOptions);
+  const markerOptions = {
+    position: { lat: 33.959874, lng: -118.459416 },
+    map: map,
+    title: 'Interceptor 007',
+    label: '1',
+    draggable: true
+  };
 
-    const infoWindow = new google.maps.InfoWindow({
-        content: '<h3>Interceptor 007</h3><p>Ballona Creek, LA, the United States of America</p><img src="/assets/interceptor007.png" alt="Interceptor007" style="width: 310px; height: auto;">'
+  const marker = new google.maps.Marker(markerOptions);
+
+  const infoWindow = new google.maps.InfoWindow({
+    content: '<h3>Interceptor 007</h3><p>Ballona Creek, LA, the United States of America</p><img src="/assets/interceptor007.png" alt="Interceptor007" style="width: 300px; height: auto;">'
+  });
+
+  marker.addListener('click', function() {
+    infoWindow.open(map, marker);
+  });
+
+  toggleButton.addEventListener('click', function() {
+    map.setZoom(7);
+  });
+
+  google.maps.event.addListener(map, 'tilesloaded', function() {
+    Promise.resolve(map).catch(function(error) {
+      console.error('An error occurred:', error);
     });
-
-    marker.addListener('click', function() {
-        infoWindow.open(map, marker);
-    });
-
-    toggleButton.addEventListener('click', function() {
-        map.setZoom(7);
-    });
-
-    google.maps.event.addListener(map, 'tilesloaded', function() {
-        Promise.resolve(map).catch(function(error) {
-            console.error('An error occurred:', error);
-        });
-    });
+  });
 }
